57ef92cb0acc9a07d8e51c251f28027920c2278b
Alexander Palamarchuk <ap@plista.com>
2016-08-05 15:12:12 +0200

    StockPlay. Step 4

--- stockplay.php
+++ stockplay.php
@@ -15,9 +15,12 @@ function calc(array $ar)
     if (count($ar) <= 1) {
         return 0;
     }
-    $min = min($ar);
-    $max = max($ar);
-    return ($max - $min);
+    $minValKeys = array_keys($ar, min($ar));
+    $maxValKeys = array_keys($ar, max($ar));
+    if (max($maxValKeys) > min($minValKeys)) {
+        return $ar[max($maxValKeys)] - $ar[min($minValKeys)];
+    }
+    return 0;
 }
 
 function assertCalc($expected, array $ar) {
@@ -36,4 +39,5 @@ assertCalc(0, [0]);
 assertCalc(0, [1=>0, 0, 0]);
 assertCalc(0, [100]);
 assertCalc(10, [90, 100]);
-
+assertCalc(30, [90, 120, 100]);
+assertCalc(0, [120, 100, 90]);
